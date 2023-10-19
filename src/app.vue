eca591e05b1d549929d9a7b4aaa714c7db929862
Jesslyn_Christania <you@example.com>
2023-10-19 09:31:49 +0800

    pushing

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <c-box>
+      <c-button>
+        Chakra Consumed! ⚡️
+      </c-button>
+    </c-box>
+  </template>
+  
+  <script lang="js">
+  import { CBox, CButton } from '@chakra-ui/vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      CBox,
+      CButton
+    }
+  }
+  </script>
