b1858dc5e5b1e6289c5bc3971d9eda690fa85d81
Jesslyn_Christania <you@example.com>
2023-10-19 18:25:00 +0800

    directory

---
+++ src/hotel/hotel.js
@@ -0,0 +1,31 @@
+const words = ["sustainable accommodations here!", "eco-friendly accomodations here!"];
+const el = document.querySelector("#typewriter");
+
+const sleepTime = 100;
+let currWordIndex = 0;
+
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+const effect = async () => {
+  while (true) {
+    currWord = words[currWordIndex];
+
+    for (let i = 0; i < currWord.length; i++) {
+      el.innerText = currWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(500);
+
+    if (currWordIndex >= words.length - 1) {
+      currWordIndex = 0;
+      await sleep(1000);
+    } else currWordIndex++;
+  }
+};
+
+effect();
+
+// for the tabs
