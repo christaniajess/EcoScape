1fb7ef7cb7bce39c6aebf4ab402a54b1d05b3390
Justin <justin.rezaba@gmail.com>
2023-10-22 01:31:27 +0800

    push

--- src/justin/New/destination.js
+++ src/justin/New/destination.js
@@ -1,3 +1,22 @@
+
+
+var cw = window.rating1.clientWidth; 
+function rating( stars ) {
+  window.rating1.style.width = Math.round(cw * (stars / 5)) + 'px';
+}
+
+axios.get('destination.json')
+                .then(response => {
+                    // this gets the data from destination.json, which is an array
+                    console.log(response.data)
+                    document.getElementById("attraction-img").setAttribute("src", response.data[0].image)
+                    document.getElementById("open-web-link").setAttribute("href", response.data[0].url)
+                    document.getElementById("attraction-name").innerText = response.data[0].name
+                    document.getElementById("rating").innerHTML = "Green Score: " + response.data[0].greenScore
+                    rating(response.data[0].rating)
+                    document.getElementById("description").innerText = response.data[0].description
+                })
+
 let map;
 
 
@@ -5,12 +24,12 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 1.3448, lng: 103.8224 },
+    center: { lat: 1.3138, lng: 103.8159 },
     zoom: 15
   });
 
   const marker = new google.maps.Marker({
-    position: { lat: 1.3448, lng: 103.8224 },
+    position: { lat: 1.3138, lng: 103.8159 },
     map: map,
     label: "A",
     title: "Random Title",
@@ -25,21 +44,3 @@ async function initMap() {
 }
 
 initMap();
-
-var cw = window.rating1.clientWidth; 
-function rating( stars ) {
-  window.rating1.style.width = Math.round(cw * (stars / 5)) + 'px';
-}
-
-axios.get('destination.json')
-                .then(response => {
-                    // this gets the data from destination.json, which is an array
-                    console.log(response.data)
-                    console.log(response.data[0].image)
-                    document.getElementById("attraction-img").setAttribute("src", response.data[0].image)
-                    document.getElementById("open-web-link").setAttribute("href", response.data[0].url)
-                    document.getElementById("attraction-name").innerText = response.data[0].name
-                    document.getElementById("rating").innerHTML = "Green Score: " + response.data[0].greenScore
-                    rating(response.data[0].rating)
-                    document.getElementById("description").innerText = response.data[0].description
-                })
