8dac886a972ab17899f84c62f2ec1cf115ac34d8
Jesslyn_Christania <you@example.com>
2023-10-28 18:37:50 +0800

    appvue

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,34 @@
 <template>
-    <c-box>
-      <c-button>
-        Chakra Consumed! ⚡️
-      </c-button>
-    </c-box>
-  </template>
-  
-  <script lang="js">
-  import { CBox, CButton } from '@chakra-ui/vue' 
-  export default {
-    name: 'App',
-    components: {
-      CBox,
-      CButton
-    }
-  }
-  
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+  <script>
+    import navbar from "@/components/navbar.vue";
+    import "aos/dist/aos.css";
+    import AOS from "aos";
+
+    export default 
+    {
+      name: "App",
+      components: 
+      {
+        navbar,
+      },
+      mounted() 
+      {
+        AOS.init(); // Initialize AOS
+      },
+  };  
+
   </script>
+<style>
+  .pageSizing 
+  {
+    min-height: 100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content: space-between;
+  }
+</style>
