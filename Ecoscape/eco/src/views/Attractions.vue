cee4b9848ccad58a3cdf5ff85e1ab2da2a51273c
Jesslyn_Christania <you@example.com>
2023-10-30 14:44:19 +0800

    tabs

--- Ecoscape/eco/src/views/Attractions.vue
+++ Ecoscape/eco/src/views/Attractions.vue
@@ -17,8 +17,8 @@
   <body>
       <NavBar/>
       <!--Header -->
-      <h2>Discover <span id="typewriter"></span><span id="cursor">|</span></h2>
-    
+        <br><br>
+        <!-- <h2>Discover <span id="typewriter"></span><span id="cursor">|</span></h2> -->
         <!--Tabs (choosing which region)-->
         <Tabs/>
     
@@ -30,6 +30,41 @@
 
 
 <script>
+
+
+    // const words = ['sustainable attactions here!','eco-friendly attractions here!'];
+    // const el = document.querySelector('#typewriter');
+    // const sleepTime = 100;
+    // let currWordIndex = 0;
+    // const sleep = (time) => 
+    // {
+    //     return new Promise((resolve)=> setTimeout(resolve,time));
+    // };
+    // const effect = async () => 
+    // {
+    //     while(true)
+    //     {
+    //         currWord = words[currWordIndex];
+    //         for(let i=0; i<currWord.length; i++)
+    //         {
+    //             el.innerText = currWord.substring(0,i+1);
+    //             await sleep(sleepTime);
+    //         }
+    //         await sleep(500);
+    //         if(currWordIndex >= words.length-1)
+    //         {
+    //             currWordIndex=0;
+    //             await sleep(1000);
+    //         }
+    //         else
+    //         {
+    //             currWordIndex++
+    //         }
+    //     }
+    // };
+    // effect();
+
+
 import NavBar from "@/components/NavBar.vue";
 import HomepageParallax from "@/components/HomepageParallax.vue";
 import Footer from "@/components/Footer.vue";
