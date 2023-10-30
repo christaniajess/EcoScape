473a22a0907f5c9221229c5cee5f07cd4f82e6bb
Jesslyn_Christania <you@example.com>
2023-10-30 13:43:25 +0800

    tabs vue

---
+++ Ecoscape/eco/src/components/Tabs.vue
@@ -0,0 +1,19 @@
+<template>
+    <input id="tab1" type="radio" name="tabs" checked>
+    <label for="tab1">All</label>
+          
+    <input id="tab2" type="radio" name="tabs">
+    <label for="tab2">Central</label>
+          
+    <input id="tab3" type="radio" name="tabs">
+    <label for="tab3">East</label>
+          
+    <input id="tab4" type="radio" name="tabs">
+    <label for="tab4">South</label>
+    
+    <input id="tab5" type="radio" name="tabs">
+    <label for="tab5">North</label>
+    
+    <input id="tab6" type="radio" name="tabs">
+    <label for="tab6">West</label>
+</template>
