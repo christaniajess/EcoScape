355cffc38e46e321d07df07bb0a55f29e58143ee
joooooly <jocelyn.yap.2022@scis.smu.edu.sg>
2023-10-31 23:37:34 +0800

    vue update

--- router/index.js
+++ router/index.js
@@ -1,19 +1,25 @@
-import { createRouter } from 'vue-router';
-import Homepage from "./src/views/Homepage.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Homepage from '@/views/Homepage.vue';
+import Login from '@/views/Login.vue';
 
-const routes = 
-[
-    {
-        path: '/',
-        name: 'Homepage',
-        component: Homepage,
-    },
-    // Add more routes here
+
+const routes = [
+  {
+    path: '/',
+    name: 'Homepage',
+    component: Homepage,
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login,
+  },
+  // Add more routes as needed
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
