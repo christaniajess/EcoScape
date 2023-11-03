7d005719aa5c73f719753c4dfb045d6cca42da5b
camille <chuayuen0122@gmail.com>
2023-11-03 19:48:51 +0800

    added co2 tracker

--- Ecoscape/src/components/Metric.vue
+++ Ecoscape/src/components/Metric.vue
@@ -1,32 +1,22 @@
 <template>
   <section id="metrics">
-    <div class="counter text-white pt-4">
-      <p>Status</p>
-      <div class="odometer text-white" ref="odometer">0</div>
-      <h6 class="title text-white">Carbon Emission Calculator</h6>
-    </div>
-  </section>
+    <iframe src="https://cdn.climatechangetracker.org/embedding/human-induced-yearly-co2-emissions?theme=dark" scrolling="no" frameBorder="0" style="width:100%; height:400px"></iframe> 
+    <div class="container-fluid text-white text-center pb-4">
+      <ul>
+        <li>Transportation sector plays a significant role in human-induced CO2 emissions</li>
+        <br>
+        <li>It includes emissions from vehicles and the production and transportation of fuels</li>
+        <br>
+
+        <li>Sustainable transportation options such as <span class="fw-bold" style="color: #afd68f;">BICYCLES, ELECTRIC VEHICLES (EVs), PUBLIC BUSES, 
+          and MASS RAPID TRANSIT (MRT) systems</span> play a significant role in decreasing the environmental impact of our daily journeys.</li>
+      </ul>
+    </div> 
+    </section>
 </template>
 
 <script>
-export default {
-  mounted() {
-    // Initialize the odometer or add any logic related to the metric here
-    this.initializeOdometer();
-  },
-  methods: {
-    initializeOdometer() {
-      // Initialize Odometer or add your logic here
-      const odometer = new Odometer({
-        el: this.$refs.odometer,
-        value: 0, // Initial value
-      });
 
-      // Update the odometer value when needed
-      // odometer.update(newValue);
-    },
-  },
-};
 </script>
 
 <style scoped>
@@ -34,36 +24,7 @@ export default {
   position: relative;
   width: 100%;
   min-height: 25vh;
+  background-color: #101207;
 }
 
-.counter{
-    position:relative;
-    align-items: center;
-    text-align: center;
-    min-height: 25vh;
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(../../images/cloud-forest.jpg)no-repeat fixed;
-    background-position:center;
-    background-size:cover;
-    z-index: 2;
-}
-.counter:before{
-    position:absolute;
-    content: "";
-    z-index: 0;
-    height:100%;
-    width:100%;
-    top:0;
-    left:0;
-}
-
-.odometer {
-  font-size: 3.5rem;
-  z-index: 2;
-}
-
-.counter.title {
-  letter-spacing: 1.5px;
-  line-height: 1.25;
-}
 </style>
