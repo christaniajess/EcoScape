2e7f20142391a7457ca20618642bec2b3598acdc
joooooly <148182986+joooooly@users.noreply.github.com>
2023-12-12 16:49:14 +0800

    remove API key

--- firebase/index.js
+++ firebase/index.js
@@ -14,15 +14,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDmN08IvkUv2vQn7o_Cbd5daeQ3OXxGRpo",
-  authDomain: "ecoscape-ea1d6.firebaseapp.com",
-  projectId: "ecoscape-ea1d6",
-  storageBucket: "ecoscape-ea1d6.appspot.com",
-  messagingSenderId: "104462396856",
-  appId: "1:104462396856:web:bcaaa850fb71afed13b34d",
-  measurementId: "G-35CF9R9L0S"
-};
+
 
 const firestore = getFirestore(); 
 
